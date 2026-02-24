4937eca93d225dd537c74f9e614e29abe27b0a94
rhoneythseabra <rhoneyholanda@gmail.com>
2026-02-24 19:59:12 -0300

    feat: decorando campos com as anotações que faltavam

--- src/main/java/com/desafio_modelo_orm/spring/entities/Bloco.java
+++ src/main/java/com/desafio_modelo_orm/spring/entities/Bloco.java
@@ -1,4 +1,42 @@
 package com.desafio_modelo_orm.spring.entities;
 
+import java.time.Instant;
+
 public class Bloco {
+
+    private Long id;
+    private Instant inicio, fim;
+
+    public Bloco() {
+    }
+
+    public Bloco(Long id, Instant inicio, Instant fim) {
+        this.id = id;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
 }
--- src/main/java/com/desafio_modelo_orm/spring/entities/Participante.java
+++ src/main/java/com/desafio_modelo_orm/spring/entities/Participante.java
@@ -1,4 +1,44 @@
 package com.desafio_modelo_orm.spring.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participante")
 public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome, email;
+
+    public Participante() {}
+
+    public Participante(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
