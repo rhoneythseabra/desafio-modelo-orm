2f2492bc01bc88c2833d23bb233b997306c4a538
rhoneythseabra <rhoneyholanda@gmail.com>
2026-03-02 12:10:09 -0300

    feat: seed completo do DB e associacoes saairam como desejado.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,28 @@
-INSERT INTO tb_atividade (nome, descricao, preco) VALUES ()
+INSERT INTO tb_categoria (descricao) VALUES ('Curso');
+INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
+
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES('Curso de HTML', 'Aprenda HTML de forma prática', 80.0, 1);
+INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES('Oficina de GitHub', 'Controle versões de seus projetos', 50.0, 2);
+
+INSERT INTO tb_participante (nome, email) VALUES ('José Silva', 'jose@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Maria do Rosário', 'maria@gmail.com');
+INSERT INTO tb_participante (nome, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
+
+INSERT INTO tb_bloco (inicio, fim, atividades_id) VALUES(TIMESTAMP '2017-09-25 08:00:00', TIMESTAMP '2017-09-25 11:00:00', 1);
+INSERT INTO tb_bloco (inicio, fim, atividades_id) VALUES(TIMESTAMP '2017-09-25 14:00:00', TIMESTAMP '2017-09-25 18:00:00', 2);
+INSERT INTO tb_bloco (inicio, fim, atividades_id) VALUES(TIMESTAMP '2017-09-26 08:00:00', TIMESTAMP '2017-09-26 11:00:00', 2);
+
+INSERT INTO tb_participante_atividade (participantes_id, atividades_id) VALUES (1, 1);
+INSERT INTO tb_participante_atividade (participantes_id, atividades_id) VALUES (2, 1);
+INSERT INTO tb_participante_atividade (participantes_id, atividades_id) VALUES (3, 1);
+INSERT INTO tb_participante_atividade (participantes_id, atividades_id) VALUES (3, 2);
+INSERT INTO tb_participante_atividade (participantes_id, atividades_id) VALUES (4, 2);
+
+
+
+
+
+
+
+
